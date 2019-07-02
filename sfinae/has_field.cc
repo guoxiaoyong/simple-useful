e7851be2238bb30c067c25a1f012e11490a97864
Xiaoyong Guo <xguo@prestolabs.io>
2019-07-02 08:20:14 +0800

    update

--- sfinae/has_field.cc
+++ sfinae/has_field.cc
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <type_traits>
 
-template <class T>
+template <typename T>
 struct HasOrderSysID {
   typedef char B[10];
-  template <typename U, U u> struct HasDataField;
-  template <typename C> struct Testr<HasDataField<B C::*, &C::OrderSysID>*) ; std::true_type {};
-  template <typename> static std::false_type test(...) {}
-
-  static const bool value = sizeof(test<T>(0)) == sizeof(yes);
+  template<typename U, U u> struct HasField {};
+  template<typename> struct Test : std::false_type {};
+  template<> struct Test<HasField<B T::*, &T::OrderSysID>> : std::true_type {};
+  static constexpr bool value = Test<T>::value;
 };
 
 struct A {
@@ -28,7 +27,7 @@ struct C {
 int main(void) {
   std::cout << HasOrderSysID<A>::value << std::endl;
   std::cout << HasOrderSysID<B>::value << std::endl;
-  std::cout << HasOrderSysID<C>::value << std::endl;
-  std::cout << HasOrderSysID<A>::test<A>() << std::endl;
+  //std::cout << HasOrderSysID<C>::value << std::endl;
+  //std::cout << HasOrderSysID<A>::test<A>() << std::endl;
   return 0;
 }
